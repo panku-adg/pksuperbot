557df69c09aa14893aad8a2d7384fe48d9e3ed4b
panku-adg <66796535+panku-adg@users.noreply.github.com>
2020-06-12 11:59:08 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,13 +29,7 @@ I'm group manager bot and Anti-scam for RR Players🃏
 Active Anti-scam protection in your BM by this command : 
 /antiscam on 
 
-My owner is [Aditya 🇮🇳](t.me/xditya)
-
-Special thanks to [this person](t.me/denver02) for helping me.
-
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
+My owner is panku 
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 """
